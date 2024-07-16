183ebdd256a03d0cb4f6683f58ce5c58bba8354c
gxres042 <i@gxres.net>
2024-07-16 20:59:16 +0800

    Preprocess data before client use

--- server/routes/linklist/index.ts
+++ server/routes/linklist/index.ts
@@ -1,11 +1,29 @@
 import yaml from "js-yaml";
 
 export default defineEventHandler(async (event) => {
-    const file = yaml.load(await useStorage("assets:server").getItem("linklist.yml"));
-    const utf8EncodedContent = new TextEncoder().encode(JSON.stringify(file, null, 2));
-    return new Response(utf8EncodedContent, {
+  const file = yaml.load(
+    await $fetch(
+      "https://friends-src.slirv.vip/list.yml", {
+        method: "GET",
         headers: {
-            "Content-Type": "application/json; charset=utf-8"
+            "User-Agent": "curl/8.2.1 (api.slirv.vip)"
         }
-    });
-})
+      }
+    )
+  );
+
+  file.forEach(item => {
+    if (item.avatar) {
+      item.avatar = `https://friends-src.slirv.vip/img/${item.avatar}`;
+    }
+  });
+
+  const jsonContent = JSON.stringify(file, null, 2);
+  const utf8EncodedContent = new TextEncoder().encode(jsonContent);
+
+  return new Response(utf8EncodedContent, {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+});
