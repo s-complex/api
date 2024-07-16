21defab7c878f546adacd86c96171430fe6cceaf
gxres042 <i@gxres.net>
2024-07-16 21:36:32 +0800

    Add fuctions (headers)

---
+++ functions/_middleware.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+export const onRequestOptions: PagesFunction = async () => {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  };
+  
