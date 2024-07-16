d102f58fcb6914afb6011db1550516b7cac0eebe
gxres042 <i@gxres.net>
2024-07-16 22:31:53 +0800

    Add CORS headers in nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,4 +1,12 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server"
+  srcDir: "server",
+  routeRules: {
+    "/linklist": {
+      cors: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+  },
 });
