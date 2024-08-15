9a7d1f2a812781d49f8d79f76db5386e5b5434ff
gxres042 <i@gxres.net>
2024-08-15 22:39:24 +0800

    Remove multiple header

--- server/routes/url-check/index.post.ts
+++ server/routes/url-check/index.post.ts
@@ -1,10 +1,8 @@
 export default defineEventHandler(async (event) => {
   const didHandleCors = handleCors(event, {
-    origin: "*",
     preflight: {
       statusCode: 204,
     },
-    methods: "*",
   });
   if (didHandleCors) {
     return;
