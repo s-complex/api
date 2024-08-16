7af033d691a1023ce3ae2674c292f7292f7c9da6
gxres042 <i@gxres.net>
2024-08-16 21:31:32 +0800

    Code cleaning

--- server/routes/url-check/index.ts
+++ server/routes/url-check/index.ts
@@ -1,23 +1,21 @@
-import { defineEventHandler, getQuery } from 'h3';
-
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
-  const target = query.check as string; // 类型断言为字符串
+  const target = query.check as string;
 
-  console.log('Received target URL:', target); // 调试信息
+  console.log('Received target URL:', target);
 
   try {
     const response = await $fetch.raw(target);
-    console.log('Response status:', response.status); // 调试信息
+    console.log('Response status:', response.status);
     return {
       statusCode: response.status,
-      statusMessage: response.statusText // 可选：返回状态消息
+      statusMessage: response.statusText
     };
   } catch (error) {
-    console.error('Error response:', error.response?.status); // 调试信息
+    console.error('Error response:', error.response?.status);
     return {
       statusCode: error.response?.status || 500,
-      statusMessage: error.response?.statusText || 'Internal Server Error' // 可选：返回错误状态消息
+      statusMessage: error.response?.statusText || 'Internal Server Error'
     };
   }
 });
